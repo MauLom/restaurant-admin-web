c4f1da4c6acdaf3d30c85cef0c3041d713cdbe5a
marcos.roman <marcos.roman@scisa.com.mx>
2024-09-16 02:05:00 -0600

    feat: Add responsive flow to product grid

--- src/components/ItemSelector.jsx
+++ src/components/ItemSelector.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { VStack, Text, IconButton, SimpleGrid, Button, Stack, Input} from '@chakra-ui/react';
-import { FaMinus } from 'react-icons/fa';
+import { VStack, Text, IconButton, SimpleGrid, Button, Stack, Input, Flex} from '@chakra-ui/react';
+import { FaTrash } from 'react-icons/fa';
 import api from '../services/api';
 
 function ItemSelector({ selectedCategory, onAddItem }) {
@@ -36,23 +36,25 @@ function ItemSelector({ selectedCategory, onAddItem }) {
       value={comment}
       onChange={(e) => setComments(e.target.value)} 
     />
-    <SimpleGrid columns={[2,3,5]} spacing={5} zIndex={900}>
-      {items.map((item) => (
-        <Stack key={item._id} spacing={0}>
-        <Button colorScheme="blue" onClick={() => handleAddItem(item._id, 1, item, comment)} height={120} p={10} borderBottomRadius={0}>
-          <VStack>
-            {/* Seria bueno mostrar una imagen de referencia para meseros nuevos les */}
-            {/* <Image src='gibbresh.png' fallbackSrc='maui-logo.png' height={150}/> */}
-            <Text textAlign='center' whiteSpace='normal'>{item.name} </Text>
-            <Text>{item.price.toFixed(2)}</Text>
-          </VStack>
-        </Button>
-        <IconButton bg='#a70000' borderTopRadius={0}
-          icon={<FaMinus />}
-          onClick={() => handleAddItem(item._id, -1, item)}
-        />
-        </Stack>
-      ))}
+    <SimpleGrid width='full' spacing='40px' marginTop={5}>
+      <Flex gap={5} flexFlow='wrap'>
+        {items.map((item) => (
+          <Stack key={item._id} spacing={0} width={200}>
+          <Button colorScheme="blue" onClick={() => handleAddItem(item._id, 1, item, comment)} height={120} p={10} borderBottomRadius={0}>
+            <VStack>
+              {/* Seria bueno mostrar una imagen de referencia para meseros nuevos les */}
+              {/* <Image src='gibbresh.png' fallbackSrc='maui-logo.png' height={150}/> */}
+              <Text textAlign='center' whiteSpace='normal'>{item.name} </Text>
+              <Text>{item.price.toFixed(2)}</Text>
+            </VStack>
+          </Button>
+          <IconButton colorScheme='red' borderTopRadius={0} 
+            icon={<FaTrash color='white'/>} 
+            onClick={() => handleAddItem(item._id, -1, item)}
+          />
+          </Stack>
+        ))}
+      </Flex>
     </SimpleGrid>
     </VStack>
   );
