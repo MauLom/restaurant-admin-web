07fc345657ee22b36c8fa7d42e86062ae501865c
Mauricio Lombano <mauricio.lombano@gmail.com>
2026-06-30 22:02:03 -0600

    correciones de eslint

--- src/shared/components/MenuItemManagement.jsx
+++ src/shared/components/MenuItemManagement.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
 import {
   Box, VStack, HStack, Button, Input, Text, Select, Grid, Image, IconButton, Collapse, Heading,
   AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter,
